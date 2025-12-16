7a6aa9bdbb01ccae35252e903f14f2e35eabbf42
KRISHNENDU SINHA <dr.krishnendusinha@gmail.com>
2025-12-16 18:44:17 +0530

    Update setup.py
    v1.0.0 to v1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="babappalign",
-    version="1.0.0",
+    version="1.0.1",
     description="Deep learning–based progressive multiple sequence alignment engine",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
